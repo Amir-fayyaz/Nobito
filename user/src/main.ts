4675f0bd83f99cd78bab873067df8362e32df909
= <=>
2025-06-10 11:12:35 -0700

    add microservice configs for user-service

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { config } from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://username:password@localhost:5672'],
+        queue: 'user_queue',
+        queueOptions: { durable: true },
+        noAck: false,
+      },
+    },
+  );
+  await app.listen();
+  Logger.log('User-service is listening on rabbit-mq');
 }
 bootstrap();
