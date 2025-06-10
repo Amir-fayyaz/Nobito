729afa0630923274888e1f6fc81f09eba3234889
= <=>
2025-06-10 10:05:56 -0700

    add env.ts file for other files

---
+++ user/src/common/constants/env.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+
+config();
+
+console.log(process.env.DB_PASSWORD);
+export const Env = {
+  //For db
+  DB_HOST: process.env.DB_HOST,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_TYPE: process.env.DB_TYPE,
+
+  //Node
+  NODE_ENV: process.env.NODE_ENV,
+};
