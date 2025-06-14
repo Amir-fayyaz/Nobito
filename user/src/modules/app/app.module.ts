c03fc859f2f6a9e7e5caf996d98db28f4b6e2ed6
= <fayamir54@gmail.com>
2025-06-14 05:50:27 -0700

    add appModule dependencies

--- user/src/modules/app/app.module.ts
+++ user/src/modules/app/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/configs/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '../user/user.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RabbitMQEnviroments } from 'src/common/constants/rabbitmq';
+import { Env } from 'src/common/constants/env';
 
 @Module({
   imports: [
@@ -9,6 +13,18 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfig,
     }),
+    UserModule,
+    ClientsModule.register([
+      {
+        name: RabbitMQEnviroments.UserService_Name,
+        transport: Transport.RMQ,
+        options: {
+          urls: [Env.RABBITMQ_URL as string],
+          queue: RabbitMQEnviroments.User_Queue,
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
   ],
   providers: [TypeOrmConfig],
 })
