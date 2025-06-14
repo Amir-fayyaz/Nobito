5e07e6c58cd87c02a516696eb515ff38d09acffb
= <fayamir54@gmail.com>
2025-06-14 05:47:54 -0700

    add gateway-app dependencies

--- gateway/src/modules/app/app.module.ts
+++ gateway/src/modules/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from '../user/user.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UserRabbitmq } from 'src/common/constants/rabbitmq';
+import { Env } from 'src/common/constants/env';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ClientsModule.register([
+      {
+        name: UserRabbitmq.UserService_Name,
+        transport: Transport.RMQ,
+        options: {
+          urls: [Env.RABBITMQ_URL as string],
+          queue: UserRabbitmq.User_Queue,
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
