81bb1a339c798fff67faded95a27feb2aedbe2e3
= <fayamir54@gmail.com>
2025-06-14 05:53:21 -0700

    add getAllUsers message-pattern

--- user/src/modules/user/user.controller.ts
+++ user/src/modules/user/user.controller.ts
@@ -1,13 +1,23 @@
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { PaginateQuery } from 'nestjs-paginate';
 import { UserService } from './user.service';
-import { UserMessages } from 'src/common/message-patterns/user-messages';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly userService: UserService,
+  ) {}
 
-  @MessagePattern(UserMessages.GETALLUSERS)
+  @MessagePattern('get_all_users')
   async getAll(@Payload() queryParams: PaginateQuery) {
-    return await this.userService.getAll(queryParams);
+    try {
+      return await this.userService.getAll(queryParams);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
