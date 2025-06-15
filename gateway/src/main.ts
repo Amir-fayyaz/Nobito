84fced051a15650245ef88637f674c532f7bd747
= <fayamir54@gmail.com>
2025-06-15 12:17:13 -0700

    add validation-pipe

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Swagger } from './configs/swagger-setup.config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConnectToMicroservices } from './configs/connectToMicroservices.config';
 import { config } from 'dotenv';
 
@@ -13,8 +13,15 @@ async function bootstrap() {
   Swagger(app);
   await ConnectToMicroservices(app);
 
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
   await app.listen(Number(process.env.APP_PORT), () => {
-    Logger.log(`Gateway service is runnung on port ${process.env.APP_PORT}`);
+    Logger.log(
+      `Gateway service is runnung on port ${process.env.APP_PORT}`,
+      'NestLogger',
+    );
   });
 }
 bootstrap();
