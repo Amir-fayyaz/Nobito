8c31b31738641f65b16415cba2594d2a782887b1
= <fayamir54@gmail.com>
2025-06-16 16:01:55 -0700

    add registerByPhone service

---
+++ user/src/modules/auth/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/modules/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { RegisterByPhoneType } from '../dto/registerByPhone.type';
+import { OtpService } from './otp.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly otpService: OtpService,
+  ) {}
+
+  async registerByPhone({ phone }: RegisterByPhoneType) {
+    const user = await this.userRepository.findOneBy({ phone });
+
+    if (user) return { status: 409 };
+
+    const { hashedOtp, otp } = await this.otpService.generateAndSend();
+
+    return {
+      otp,
+      hashedOtp,
+    };
+  }
+}
