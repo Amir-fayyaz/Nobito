a02383233bbc1926c988e7a4e397745b41bc019d
= <fayamir54@gmail.com>
2025-06-16 07:45:03 -0700

    add modules to app

--- gateway/src/modules/app/app.module.ts
+++ gateway/src/modules/app/app.module.ts
@@ -5,10 +5,14 @@ import { UserModule } from '../user/user.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { UserRabbitmq } from 'src/common/constants/rabbitmq';
 import { Env } from 'src/common/constants/env';
+import { RedisModule } from 'src/common/modules/redis.module';
+import { AppCacheModule } from 'src/common/modules/cache.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     ClientsModule.register([
       {
         name: UserRabbitmq.UserService_Name,
@@ -20,6 +24,8 @@ import { Env } from 'src/common/constants/env';
         },
       },
     ]),
+    RedisModule.forRootAsync(),
+    AppCacheModule,
   ],
   controllers: [AppController],
   providers: [AppService],
