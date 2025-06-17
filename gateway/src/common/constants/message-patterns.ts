a5bb32a3596a835594425d05fd4f6068f8bf64ac
= <fayamir54@gmail.com>
2025-06-17 04:29:21 -0700

    add register-by-phone message-pattern

--- gateway/src/common/constants/message-patterns.ts
+++ gateway/src/common/constants/message-patterns.ts
@@ -2,3 +2,7 @@ export enum UserMessagePattern {
   GETALLUSERS = 'get_all_users',
   GET_USER_BY_ID = 'get_user_by_id',
 }
+
+export enum AuthMessagePattern {
+  REGISTER_BY_PHONE = 'register_by_phone',
+}
