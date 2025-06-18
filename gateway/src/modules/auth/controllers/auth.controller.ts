6da2cb2e97213dcfe624502ab27ca2f8f22ba6ee
= <fayamir54@gmail.com>
2025-06-18 06:23:27 -0700

    add register-by-email api

--- gateway/src/modules/auth/controllers/auth.controller.ts
+++ gateway/src/modules/auth/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { RegisterByPhone } from '../dto/register-by-phone.dto';
 import { VerifyByPhoneDto } from '../dto/verify-by-phone.dto';
+import { RegisterByEmail } from '../dto/register-by-email.dto';
 
 @Controller('api/v1/auth')
 export class AuthController {
@@ -15,5 +16,10 @@ export class AuthController {
   @Post('verify_by_phone')
   async verifyByPhone(@Body() dto: VerifyByPhoneDto) {
     return await this.AuthService.verifyByPhone(dto);
+  }
+
+  @Post('register_by_email')
+  async registerByEmail(@Body() dto: RegisterByEmail) {
+    return await this.AuthService.registerByEmail(dto);
   }
 }
