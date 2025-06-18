8daf397d05b9ea4fa7e13855f8674f5326e601c8
= <fayamir54@gmail.com>
2025-06-18 10:49:41 -0700

    Add create-role serivce

---
+++ gateway/src/modules/auth/services/role.service.ts
@@ -0,0 +1,26 @@
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { UserRabbitmq } from 'src/common/constants/rabbitmq';
+import { CreateRoleDto } from '../dto/create-role.dto';
+import { lastValueFrom } from 'rxjs';
+import { RoleMessagePattern } from 'src/common/constants/message-patterns';
+
+@Injectable()
+export class RoleService {
+  constructor(
+    @Inject(UserRabbitmq.UserService_Name)
+    private readonly userClient: ClientProxy,
+  ) {}
+
+  async create(dto: CreateRoleDto) {
+    const newRole = await lastValueFrom(
+      this.userClient.send(RoleMessagePattern.CREATE_ROLE, dto),
+    );
+
+    if (newRole.status === 409) throw new ConflictException();
+
+    return {
+      role: newRole,
+    };
+  }
+}
