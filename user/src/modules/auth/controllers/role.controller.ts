413e12d7aed10a25818de0abe4ae03dd0d12415d
= <fayamir54@gmail.com>
2025-06-18 10:48:34 -0700

    Inject role-repository

--- user/src/modules/auth/controllers/role.controller.ts
+++ user/src/modules/auth/controllers/role.controller.ts
@@ -2,9 +2,15 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RoleService } from '../services/role.service';
 import { RoleMessage } from 'src/common/message-patterns/role-message';
 import { CreateRole } from '../dto/create-role.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from '../entities/role.entity';
+import { Repository } from 'typeorm';
 
 export class RoleController {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(
+    private readonly roleService: RoleService,
+    @InjectRepository(Role) private readonly roleRepository: Repository<Role>,
+  ) {}
 
   @MessagePattern(RoleMessage.CREATE_ROLE)
   async create(@Payload() dto: CreateRole) {
