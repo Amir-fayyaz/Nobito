e81acfd494f7a999e4b5b02075520002ea1c6738
= <fayamir54@gmail.com>
2025-06-18 10:26:55 -0700

    add dependencies

--- user/src/modules/auth/auth.module.ts
+++ user/src/modules/auth/auth.module.ts
@@ -7,11 +7,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import { RoleController } from './controllers/role.controller';
+import { RoleService } from './services/role.service';
+import { Role } from './entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [AuthController],
-  providers: [AuthService, OtpService, JwtAppService, JwtService, UserService],
+  imports: [TypeOrmModule.forFeature([User, Role])],
+  controllers: [AuthController, RoleController],
+  providers: [
+    AuthService,
+    OtpService,
+    JwtAppService,
+    JwtService,
+    UserService,
+    RoleService,
+  ],
   exports: [JwtService],
 })
 export class AuthModule {}
