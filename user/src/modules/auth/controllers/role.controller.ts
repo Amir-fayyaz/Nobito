be87a5db556bb74c7d202450f7a85f3322c1a601
= <fayamir54@gmail.com>
2025-06-18 22:14:13 -0700

    Add fineOneById message-pattern

--- user/src/modules/auth/controllers/role.controller.ts
+++ user/src/modules/auth/controllers/role.controller.ts
@@ -5,6 +5,7 @@ import { CreateRole } from '../dto/create-role.type';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from '../entities/role.entity';
 import { Repository } from 'typeorm';
+import { FindRoleById } from '../dto/fine-one-by-id.type';
 
 export class RoleController {
   constructor(
@@ -20,5 +21,10 @@ export class RoleController {
   @MessagePattern(RoleMessage.GET_ALL_ROLES)
   async findAll() {
     return await this.roleService.findAll();
+  }
+
+  @MessagePattern(RoleMessage.FIND_ROLE_BY_ID)
+  async findOne(@Payload() { id }: FindRoleById) {
+    return await this.roleService.findOne(id);
   }
 }
