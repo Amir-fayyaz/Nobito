f3d2b20b099f928e5e8b6598c5c2c9ac13688e44
= <fayamir54@gmail.com>
2025-06-20 04:47:09 -0700

    Add position dependencies

--- user/src/modules/auth/auth.module.ts
+++ user/src/modules/auth/auth.module.ts
@@ -10,10 +10,13 @@ import { UserService } from '../user/user.service';
 import { RoleController } from './controllers/role.controller';
 import { RoleService } from './services/role.service';
 import { Role } from './entities/role.entity';
+import { PositionController } from './controllers/position.controller';
+import { PositionService } from './services/position.service';
+import { Position } from './entities/position.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
-  controllers: [AuthController, RoleController],
+  imports: [TypeOrmModule.forFeature([User, Role, Position])],
+  controllers: [AuthController, RoleController, PositionController],
   providers: [
     AuthService,
     OtpService,
@@ -21,6 +24,7 @@ import { Role } from './entities/role.entity';
     JwtService,
     UserService,
     RoleService,
+    PositionService,
   ],
   exports: [JwtService],
 })
