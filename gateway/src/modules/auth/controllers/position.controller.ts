e5f252f149620944a954c9f3c6b171d1ad8c1f8b
= <fayamir54@gmail.com>
2025-06-20 06:36:30 -0700

    Add create-position api

---
+++ gateway/src/modules/auth/controllers/position.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { PositionService } from '../services/position.service';
+import { CreatePositionDto } from '../dto/create-position.dto';
+
+@Controller('api/v1/positions')
+export class PositionController {
+  constructor(private readonly PositionService: PositionService) {}
+
+  @Post()
+  async create(@Body() dto: CreatePositionDto) {
+    return await this.PositionService.create(dto);
+  }
+}
---
+++ user/src/modules/auth/controllers/position.controller.ts
@@ -0,0 +1,20 @@
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Position } from '../entities/position.entity';
+import { Repository } from 'typeorm';
+import { PositionService } from '../services/position.service';
+import { PositionMessage } from 'src/common/message-patterns/position-messages';
+import { CreatePositon } from '../dto/create-position.type';
+
+export class PositionController {
+  constructor(
+    @InjectRepository(Position)
+    private positionRepository: Repository<Position>,
+    private readonly positionService: PositionService,
+  ) {}
+
+  @MessagePattern(PositionMessage.CREATE_POSITION)
+  async create(@Payload() dto: CreatePositon) {
+    return await this.positionService.create(dto);
+  }
+}
