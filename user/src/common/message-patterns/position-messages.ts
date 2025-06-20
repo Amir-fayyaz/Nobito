8796bbda24b5105af4502c0af46e5d9e2e8c4420
= <fayamir54@gmail.com>
2025-06-20 07:02:43 -0700

    Add message patterns

--- user/src/common/message-patterns/position-messages.ts
+++ user/src/common/message-patterns/position-messages.ts
@@ -2,4 +2,6 @@ export enum PositionMessage {
   CREATE_POSITION = 'create_position',
   GET_ALL_POSITIONS = 'get_all_position',
   FIND_ONE_BY_ID = 'get_position_by_id',
+  UPDATE_POSITION = 'update_position',
+  DELETE_POSITION = 'delete_position',
 }
