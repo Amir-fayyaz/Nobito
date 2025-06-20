4321b3332f79c4d4d36ee875c18c5268fcea78be
= <fayamir54@gmail.com>
2025-06-20 11:57:32 -0700

    Add position-messages

--- gateway/src/common/constants/message-patterns.ts
+++ gateway/src/common/constants/message-patterns.ts
@@ -23,4 +23,6 @@ export enum PositionMessagePattern {
   CREATE_POSITION = 'create_position',
   GET_ALL_POSITIONS = 'get_all_position',
   FIND_ONE_BY_ID = 'get_position_by_id',
+  UPDATE_POSITION = 'update_position',
+  DELETE_POSITION = 'delete_position',
 }
