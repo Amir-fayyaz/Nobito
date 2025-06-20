1bd1626e4ce63f0a93391cebb6f714c6271f9979
= <fayamir54@gmail.com>
2025-06-20 06:37:15 -0700

    Add create-position service

---
+++ gateway/src/modules/auth/services/position.service.ts
@@ -0,0 +1,26 @@
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { UserRabbitmq } from 'src/common/constants/rabbitmq';
+import { CreatePositionDto } from '../dto/create-position.dto';
+import { lastValueFrom } from 'rxjs';
+import { PositionMessagePattern } from 'src/common/constants/message-patterns';
+
+@Injectable()
+export class PositionService {
+  constructor(
+    @Inject(UserRabbitmq.UserService_Name)
+    private readonly userClient: ClientProxy,
+  ) {}
+
+  async create(dto: CreatePositionDto) {
+    const createResult = await lastValueFrom(
+      this.userClient.send(PositionMessagePattern.CREATE_POSITION, dto),
+    );
+
+    if (createResult.status === 409) throw new ConflictException();
+
+    return {
+      position: createResult,
+    };
+  }
+}
