c17557e4b837c841db71254b94e4463330af9ec3
= <fayamir54@gmail.com>
2025-06-22 07:18:56 -0700

    Add Personnel-messages

---
+++ user/src/common/message-patterns/personnel-messages.ts
@@ -0,0 +1,7 @@
+export enum PersonnelMessage {
+  CREATE_PERSONNEL = 'create_personnel',
+  GET_ALL_PERSONNEL = 'get_all_personnel',
+  GET_PERSONNEL_BY_ID = 'get_personnel_by_id',
+  UPDATE_PERSONNEL = 'update_personnel',
+  DELETE_PERSONNEL = 'delete_personnel',
+}
