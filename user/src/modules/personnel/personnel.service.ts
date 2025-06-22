336ec9624046e61cd6db62a3d288838a7de05501
= <fayamir54@gmail.com>
2025-06-22 07:21:32 -0700

    Add create-personnel service

---
+++ user/src/modules/personnel/personnel.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Personnel } from './entities/personnel.entity';
+import { DeepPartial, Repository } from 'typeorm';
+import { CreatePersonnel } from './dto/create-personnel.type';
+import { generatePersonnelNumber } from 'src/common/utility/set-personnel-number';
+
+@Injectable()
+export class PersonnelService {
+  constructor(
+    @InjectRepository(Personnel)
+    private readonly personnelRepository: Repository<Personnel>,
+  ) {}
+
+  async create(dto: CreatePersonnel): Promise<DeepPartial<Personnel>> {
+    const newPersonnel: DeepPartial<Personnel> =
+      this.personnelRepository.create(dto);
+    const savedPersonnel = await this.personnelRepository.save(newPersonnel);
+
+    const personnelNumber: string = generatePersonnelNumber(1);
+    savedPersonnel.PersonnelNumber = personnelNumber;
+
+    return await this.personnelRepository.save(savedPersonnel);
+  }
+}
