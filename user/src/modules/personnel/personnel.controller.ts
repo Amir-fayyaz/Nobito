0037caf238be277bd871e925cc89f4c532c66a39
= <fayamir54@gmail.com>
2025-06-23 16:17:47 -0700

    Inject repository

--- user/src/modules/personnel/personnel.controller.ts
+++ user/src/modules/personnel/personnel.controller.ts
@@ -2,9 +2,16 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { PersonnelService } from './personnel.service';
 import { PersonnelMessage } from 'src/common/message-patterns/personnel-messages';
 import { CreatePersonnel } from './dto/create-personnel.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Personnel } from './entities/personnel.entity';
+import { Repository } from 'typeorm';
 
 export class PersonnelController {
-  constructor(private readonly personnelService: PersonnelService) {}
+  constructor(
+    private readonly personnelService: PersonnelService,
+    @InjectRepository(Personnel)
+    private readonly personnelRepo: Repository<Personnel>,
+  ) {}
 
   @MessagePattern(PersonnelMessage.CREATE_PERSONNEL)
   async create(@Payload() dto: CreatePersonnel) {
