ddfd7d1a55a9e451929f1f38fe2f967eca9a7bd1
= <fayamir54@gmail.com>
2025-06-24 08:03:30 -0700

    Add findOneById api

--- gateway/src/modules/personnel/personnel.controller.ts
+++ gateway/src/modules/personnel/personnel.controller.ts
@@ -1,6 +1,9 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
+  ParseIntPipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -9,7 +12,7 @@ import { ApiBody, ApiConsumes, ApiOkResponse } from '@nestjs/swagger';
 import { CreatePersonnelDto } from './dto/create-personnel.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { PersonnelService } from './personnel.service';
-import { CreatePersonnelResponse } from './models/create-personnel-response';
+import { Personnel } from './models/personnel.model';
 
 @Controller('api/v1/personnel')
 export class PersonnelController {
@@ -17,10 +20,16 @@ export class PersonnelController {
 
   @ApiConsumes('multipart/form-data')
   @ApiBody({ type: CreatePersonnelDto })
-  @ApiOkResponse({ type: CreatePersonnelResponse })
+  @ApiOkResponse({ type: Personnel })
   @Post()
   @UseInterceptors(FileInterceptor('resume'))
   async create(@Body() dto: any, @UploadedFile() resume: Express.Multer.File) {
     return await this.personnelService.create(dto, resume);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: Personnel })
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.personnelService.findOneById(id);
   }
 }
