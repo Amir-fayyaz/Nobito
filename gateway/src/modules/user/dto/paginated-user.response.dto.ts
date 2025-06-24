c094e548e978ba7ccc462f9c5244ecb74c6b2593
= <fayamir54@gmail.com>
2025-06-24 09:35:11 -0700

    Add paginate-user-response type

---
+++ gateway/src/modules/user/dto/paginated-user.response.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../models/user.model';
+import { PaginationResponse } from 'src/common/dto/paginate-response.dto';
+
+export class PaginatedUserResponse extends PaginationResponse {
+  @ApiProperty({ type: [User] })
+  data: User[];
+}
--- gateway/src/modules/user/user.controller.ts
+++ gateway/src/modules/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { PaginatedUserResponse } from './dto/paginated-user.response.dto';
 
 @Controller('api/v1/user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
   @Get()
+  @ApiOkResponse({ type: PaginatedUserResponse })
   async findAll(@Paginate() query: PaginateQuery) {
     return await this.UserService.findAll(query);
   }
