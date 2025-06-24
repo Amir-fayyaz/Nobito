00e662c0d5520155999074c7e0d8bdf940498f15
= <fayamir54@gmail.com>
2025-06-24 10:06:45 -0700

    Add findAll-personnel Api

--- gateway/src/modules/personnel/personnel.controller.ts
+++ gateway/src/modules/personnel/personnel.controller.ts
@@ -13,6 +13,9 @@ import { CreatePersonnelDto } from './dto/create-personnel.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { PersonnelService } from './personnel.service';
 import { Personnel } from './models/personnel.model';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { PaginatePersonnelResponse } from './dto/paginate-personnel-response.dto';
+import { PaginationOptions } from 'src/common/decorators/pagination-options.decorator';
 
 @Controller('api/v1/personnel')
 export class PersonnelController {
@@ -25,6 +28,19 @@ export class PersonnelController {
   @UseInterceptors(FileInterceptor('resume'))
   async create(@Body() dto: any, @UploadedFile() resume: Express.Multer.File) {
     return await this.personnelService.create(dto, resume);
+  }
+
+  @Get()
+  @PaginationOptions({
+    filterOptions: [
+      { field: 'positionId', example: '$eq:23' },
+      { field: 'salary_amount', example: '$eq:2300000 , $lt:1000000' },
+    ],
+    sortOptions: [{ example: 'createdAt:DESC' }, { example: 'createdAt:ASC' }],
+  })
+  @ApiOkResponse({ type: PaginatePersonnelResponse })
+  async findAll(@Paginate() query: PaginateQuery) {
+    return await this.personnelService.findAll(query);
   }
 
   @Get(':id')
