610886abd4e84bf307aa6e45e7c2c9630db94bea
= <fayamir54@gmail.com>
2025-06-28 14:38:39 -0700

    Add create-doctor service

---
+++ appointment/src/modules/doctor/doctor.service.ts
@@ -0,0 +1,42 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Doctor } from './entities/doctor.entity';
+import { Repository } from 'typeorm';
+import { CreateDoctor } from './dto/create-doctor.dto';
+import { RabbitmqEnviroments } from 'src/common/constants/rabbitmq';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { PersonnelMessage } from 'src/common/constants/message-patterns/personnel.messages';
+import { generateDoctorNumber } from 'src/common/utility/set-doctorNumber';
+
+@Injectable()
+export class DoctorService {
+  constructor(
+    @InjectRepository(Doctor)
+    private readonly doctorRepository: Repository<Doctor>,
+    @Inject(RabbitmqEnviroments.UserService_Name)
+    private readonly userClient: ClientProxy,
+  ) {}
+
+  async create(dto: CreateDoctor) {
+    try {
+      await this.isPersonnelExist(dto.personnelId);
+      const newDoctor = this.doctorRepository.create(dto);
+      const savedDoctor = await this.doctorRepository.save(newDoctor);
+
+      savedDoctor.doctorNumber = generateDoctorNumber(savedDoctor.id);
+      return await this.doctorRepository.save(savedDoctor);
+    } catch (error) {
+      return { message: error.message, status: 404 };
+    }
+  }
+
+  private async isPersonnelExist(personnelId: number) {
+    const personnel: boolean = await lastValueFrom(
+      this.userClient.send(PersonnelMessage.IS_EXIST, { id: personnelId }),
+    );
+
+    if (!personnel)
+      throw new NotFoundException('There is no personnel with this id');
+  }
+}
