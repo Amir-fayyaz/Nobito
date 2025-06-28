cac5cee31b8244d352722ed8e8032ccaec1e5b82
= <fayamir54@gmail.com>
2025-06-28 14:45:42 -0700

    add doctor message-patterns

--- gateway/src/common/constants/message-patterns.ts
+++ gateway/src/common/constants/message-patterns.ts
@@ -38,3 +38,11 @@ export enum PersonnelMessagePattern {
   UPDATE_PERSONNEL = 'update_personnel',
   DELETE_PERSONNEL = 'delete_personnel',
 }
+
+export enum DoctorMessagePattern {
+  CREATE_DOCTOR = 'create_doctor',
+  UPDATE_DOCTOR = 'update_doctor',
+  FIND_ALL_DOCTOR = 'find_all_doctor',
+  FIND_ONE_DOCTOR = 'find_one_doctor',
+  DELETE_DOCTOR = 'delete_doctor',
+}
