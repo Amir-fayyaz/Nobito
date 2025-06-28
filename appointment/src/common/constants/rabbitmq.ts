3333e879edb090be45ec9e5f044368b5eb55377b
= <fayamir54@gmail.com>
2025-06-28 09:55:06 -0700

    Add user-rabbitmq enviroments

--- appointment/src/common/constants/rabbitmq.ts
+++ appointment/src/common/constants/rabbitmq.ts
@@ -1,4 +1,6 @@
 export enum RabbitmqEnviroments {
   Appointment_queue = 'appointment_queue',
   AppointmentService_Name = 'APPOINTMENT_SERVICE',
+  User_Queue = 'user_queue',
+  UserService_Name = 'USER_SERVICE',
 }
