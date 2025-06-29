d924b454269e118cb0aa565892aef93326cee873
= <fayamir54@gmail.com>
2025-06-29 08:41:39 -0700

    Add findAll-doctor end-point

--- gateway/src/modules/doctor/doctor.controller.ts
+++ gateway/src/modules/doctor/doctor.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DoctorService } from './doctor.service';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { PaginationOptions } from 'src/common/decorators/pagination-options.decorator';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { PaginateDoctorResponse } from './dto/paginate-doctor-response.dto';
 
 @Controller('api/v1/doctors')
 export class DoctorController {
@@ -9,5 +13,14 @@ export class DoctorController {
   @Post()
   async create(@Body() dto: CreateDoctorDto) {
     return await this.doctorService.create(dto);
+  }
+
+  @Get()
+  @PaginationOptions({
+    sortOptions: [{ example: 'createdAt:DESC' }, { example: 'updatedAt:DESC' }],
+  })
+  @ApiOkResponse({ type: PaginateDoctorResponse })
+  async findAll(@Paginate() query: PaginateQuery) {
+    return await this.doctorService.findAll(query);
   }
 }
