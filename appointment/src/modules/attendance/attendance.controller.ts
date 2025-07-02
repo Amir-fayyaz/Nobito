b8a91195468d5f9f7160af7124878f8b1cac4db4
= <fayamir54@gmail.com>
2025-07-02 09:37:27 -0700

    Add create-attendance end-point

---
+++ appointment/src/modules/attendance/attendance.controller.ts
@@ -0,0 +1,20 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Attendance } from './entities/attendance.entity';
+import { Repository } from 'typeorm';
+import { AttendanceService } from './attendance.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { AttendanceMessage } from 'src/common/constants/message-patterns/attendance.messages';
+import { CreateAttendance } from './dto/create-attendance.type';
+
+export class AttendanceController {
+  constructor(
+    @InjectRepository(Attendance)
+    private readonly attendanceRepository: Repository<Attendance>,
+    private readonly attendanceService: AttendanceService,
+  ) {}
+
+  @MessagePattern(AttendanceMessage.CREATE_ATTENDANCE)
+  async create(@Payload() dto: CreateAttendance) {
+    return await this.attendanceService.create(dto);
+  }
+}
