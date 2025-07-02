6ada07ab02ceb3559a9c322f003ffe4dc2da7651
= <fayamir54@gmail.com>
2025-07-02 09:18:14 -0700

    Add attendance messages

---
+++ appointment/src/common/constants/message-patterns/attendance.messages.ts
@@ -0,0 +1,7 @@
+export enum AttendanceMessage {
+  FIND_ALL_ATTENDANCE = 'find_all_attendance',
+  FIND_ONE_ATTENDANCE = 'find_one_attendance',
+  CREATE_ATTENDANCE = 'create_attendance',
+  UPDATE_ATTENDANCE = 'update_attendance',
+  DELETE_ATTENDANCE = 'delete_attendance',
+}
