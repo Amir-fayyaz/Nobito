a2839cdc33f109be1513d766f6f13e6f2a9ce0a7
= <fayamir54@gmail.com>
2025-07-02 09:36:00 -0700

    Add create-attendance service

---
+++ appointment/src/modules/attendance/attendance.service.ts
@@ -0,0 +1,40 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Attendance } from './entities/attendance.entity';
+import { Repository } from 'typeorm';
+import { CreateAttendance } from './dto/create-attendance.type';
+import { PersonnelMessage } from 'src/common/constants/message-patterns/personnel.messages';
+import { lastValueFrom } from 'rxjs';
+import { RabbitmqEnviroments } from 'src/common/constants/rabbitmq';
+import { ClientProxy } from '@nestjs/microservices';
+import { execptionError } from 'src/common/@types/eception.type';
+
+@Injectable()
+export class AttendanceService {
+  constructor(
+    @InjectRepository(Attendance)
+    private readonly attendanceRespository: Repository<Attendance>,
+    @Inject(RabbitmqEnviroments.UserService_Name)
+    private readonly userClient: ClientProxy,
+  ) {}
+
+  async create(dto: CreateAttendance): Promise<Attendance | execptionError> {
+    try {
+      await this.isPersonnelExist(dto.personnelId);
+    } catch (error) {
+      return { status: 404, message: error.message };
+    }
+
+    const newAttendance = this.attendanceRespository.create(dto);
+    return await this.attendanceRespository.save(newAttendance);
+  }
+
+  private async isPersonnelExist(personnelId: number): Promise<void> {
+    const personnel: boolean = await lastValueFrom(
+      this.userClient.send(PersonnelMessage.IS_EXIST, { id: personnelId }),
+    );
+
+    if (!personnel)
+      throw new NotFoundException('There is no personnel with this id');
+  }
+}
