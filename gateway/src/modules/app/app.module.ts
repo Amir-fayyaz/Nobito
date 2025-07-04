c93ba6f106332e3f3802f86c48c6357b353ea812
= <fayamir54@gmail.com>
2025-07-04 06:55:52 -0700

    Add attendance module

--- gateway/src/modules/app/app.module.ts
+++ gateway/src/modules/app/app.module.ts
@@ -2,36 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { UserRabbitmq } from 'src/common/constants/rabbitmq';
-import { Env } from 'src/common/constants/env';
 import { RedisModule } from 'src/common/modules/redis.module';
 import { AppCacheModule } from 'src/common/modules/cache.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { PersonnelModule } from '../personnel/personnel.module';
 import { DoctorModule } from '../doctor/doctor.module';
+import { AttendanceModule } from '../attendance/attendance.module';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    ClientsModule.register([
-      {
-        name: UserRabbitmq.UserService_Name,
-        transport: Transport.RMQ,
-        options: {
-          urls: [Env.RABBITMQ_URL as string],
-          queue: UserRabbitmq.User_Queue,
-          queueOptions: { durable: true },
-        },
-      },
-    ]),
     RedisModule.forRootAsync(),
     AppCacheModule,
     AuthModule,
     PersonnelModule,
     DoctorModule,
+    AttendanceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
