308911f21dd2b933824f80733ef8137f47bcad6a
= <fayamir54@gmail.com>
2025-07-07 07:30:31 -0700

    Add findOne treatment-category end-point

---
+++ appointment/src/modules/treatment-category/dto/find-one-treatmentCategory.type.ts
@@ -0,0 +1,3 @@
+export interface FindOneTreatmentCategory {
+  id: number;
+}
--- appointment/src/modules/treatment-category/treatmentCategory.controller.ts
+++ appointment/src/modules/treatment-category/treatmentCategory.controller.ts
@@ -6,6 +6,7 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { TreatmentCategoryMessage } from 'src/common/constants/message-patterns/treatment-category.messages';
 import { CreateTreatmentCategory } from './dto/create-treatmentCategory.type';
 import { PaginateQuery } from 'nestjs-paginate';
+import { FindOneTreatmentCategory } from './dto/find-one-treatmentCategory.type';
 
 export class TreatmentCategoryController {
   constructor(
@@ -22,5 +23,10 @@ export class TreatmentCategoryController {
   @MessagePattern(TreatmentCategoryMessage.FINDALL_TREATMENT_CATEGORY)
   async findAll(@Payload() query: PaginateQuery) {
     return await this.treatmentCategoryService.findAll(query);
+  }
+
+  @MessagePattern(TreatmentCategoryMessage.FIND_ONE_TREATMENT_CATEGORY)
+  async findOne(@Payload() { id }: FindOneTreatmentCategory) {
+    return await this.treatmentCategoryService.findOne(id);
   }
 }
