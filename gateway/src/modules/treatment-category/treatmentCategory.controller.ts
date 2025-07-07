a448407d7d15ab70df371e5df534d69ba33d89a9
= <fayamir54@gmail.com>
2025-07-07 07:19:30 -0700

    Add findAll treatment-category end-point

--- gateway/src/modules/treatment-category/treatmentCategory.controller.ts
+++ gateway/src/modules/treatment-category/treatmentCategory.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TreatmentCategoryService } from './treatmentCategory.service';
 import { CreateTreatmentCategoryDto } from './dto/create-treatmentCategory.dto';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { TreatmentCategory } from './models/treatmentCategory.model';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { PaginateTreatmentCategoryResponse } from './dto/paginate-treatmentCategory-response.dto';
+import { PaginationOptions } from 'src/common/decorators/pagination-options.decorator';
 
 @Controller('api/v1/treatment-category')
 export class TreatmentCategoryController {
@@ -14,5 +17,14 @@ export class TreatmentCategoryController {
   @ApiOkResponse({ type: TreatmentCategory })
   async create(@Body() dto: CreateTreatmentCategoryDto) {
     return await this.treatmentCategoryService.create(dto);
+  }
+
+  @Get()
+  @PaginationOptions({
+    sortOptions: [{ example: 'createdAt:ASC' }],
+  })
+  @ApiOkResponse({ type: PaginateTreatmentCategoryResponse })
+  async findAll(@Paginate() query: PaginateQuery) {
+    return await this.treatmentCategoryService.findAll(query);
   }
 }
