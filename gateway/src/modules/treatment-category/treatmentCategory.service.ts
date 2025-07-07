06173750d826b926ed59553667b658e1007bc807
= <fayamir54@gmail.com>
2025-07-07 07:12:50 -0700

    Add findAll treatment-category service

--- gateway/src/modules/treatment-category/treatmentCategory.service.ts
+++ gateway/src/modules/treatment-category/treatmentCategory.service.ts
@@ -7,6 +7,7 @@ import { TreatmentCategoryMessagePattern } from 'src/common/constants/message-pa
 import { TreatmentCategory } from './models/treatmentCategory.model';
 import { exeptionFilter } from 'src/common/filters/exeption-filter';
 import { Exeption } from 'src/common/@types/exeption-type.type';
+import { Paginated, PaginateQuery } from 'nestjs-paginate';
 
 @Injectable()
 export class TreatmentCategoryService {
@@ -17,16 +18,27 @@ export class TreatmentCategoryService {
 
   async create(
     dto: CreateTreatmentCategoryDto,
-  ): Promise<TreatmentCategory | Exeption | undefined> {
+  ): Promise<TreatmentCategory | Exeption | void> {
     try {
-      return await lastValueFrom(
+      const result = await lastValueFrom(
         this.appointmentClient.send(
           TreatmentCategoryMessagePattern.CREATE_TREATMENT_CATEGORY,
           dto,
         ),
       );
+      if (result.status) exeptionFilter(result.status, result.message);
+      return result;
     } catch (e) {
       exeptionFilter(400, e.message);
     }
+  }
+
+  async findAll(query: PaginateQuery): Promise<Paginated<TreatmentCategory>> {
+    return await lastValueFrom(
+      this.appointmentClient.send(
+        TreatmentCategoryMessagePattern.FINDALL_TREATMENT_CATEGORY,
+        query,
+      ),
+    );
   }
 }
