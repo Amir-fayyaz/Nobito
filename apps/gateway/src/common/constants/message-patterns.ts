918c4d7b611da1b0948e19910da37cb9d7d13f18
= <fayamir54@gmail.com>
2025-07-13 11:11:34 -0700

    Add patient message patterns

--- apps/gateway/src/common/constants/message-patterns.ts
+++ apps/gateway/src/common/constants/message-patterns.ts
@@ -62,3 +62,11 @@ export enum TreatmentCategoryMessagePattern {
   UPDATE_TREATMENT_CATEGORY = 'update_treatment_cateogry',
   DELETE_TREATMENT_CATEGORY = 'delete_treatment_cateogry',
 }
+
+export enum PatientMessage {
+  CREATE = 'create_patient',
+  FIND_ALL = 'find_all_patient',
+  FIND_ONE = 'find_one_patient',
+  UPDATE = 'update_patient',
+  DELETE = 'delete_patient',
+}
