7fadc0ff09b38339fc891591ae5c1fe3e3e9556c
= <fayamir54@gmail.com>
2025-07-14 06:20:37 -0700

    Add doctor-messages

---
+++ libs/message-patterns/doctor.messages.ts
@@ -0,0 +1,7 @@
+export enum DoctorMessage {
+  CREATE = 'create_doctor',
+  UPDATE = 'update_doctor',
+  FIND_ALL = 'find_all_doctor',
+  FIND_ONE = 'find_one_doctor',
+  DELETE_ = 'delete_doctor',
+}
