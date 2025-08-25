a16d5fb6b740a3c137e3daafddfb607ec92c9ef5
= <fayamir54@gmail.com>
2025-08-25 07:54:43 -0700

    add new dependecies

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,16 @@
+import { CacheService } from '@common/services/cache.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './controllers/auth.controller';
 import { User } from './entities/user.entity';
+import { AuthService } from './services/auth.service';
+import { OtpService } from './services/otp.service';
 import { UserService } from './services/user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [UserService],
+  providers: [UserService, CacheService, OtpService, AuthService],
   exports: [],
 })
 export class AuthModule {}
