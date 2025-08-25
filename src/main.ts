361b1db7204bb7b395bac1fa28e556d61380d0d7
= <fayamir54@gmail.com>
2025-08-25 07:23:25 -0700

    add api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
-import { Swagger } from './configs/swagger-setup.config';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConnectToMicroservices } from './configs/connectToMicroservices.config';
+import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
+import { Swagger } from './configs/swagger-setup.config';
+import { AppModule } from './modules/app/app.module';
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   Swagger(app);
-  await ConnectToMicroservices(app);
 
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
 
   await app.listen(Number(process.env.APP_PORT), () => {
