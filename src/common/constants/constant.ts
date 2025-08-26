350f9025eeb0fbe1c6e95634de5cfd13fdd8550c
= <fayamir54@gmail.com>
2025-08-26 05:53:16 -0700

    add jwt constants

--- src/common/constants/constant.ts
+++ src/common/constants/constant.ts
@@ -1,2 +1,8 @@
-export const JwtAccess_Expire = '30m';
+export const JwtAccess_Expire = '30m'; //for development
 export const JwtRefresh_Expire = '7d';
+
+export const JwtAccess_Name = 'X-NOBITO-ACCESS-TOKEN';
+export const JwtRefresh_Name = 'X-NOBITO-REFRESH-TOKEN';
+
+export const MaxAge_RefreshToken = 30 * 24 * 60 * 60 * 1000;
+export const MaxAge_AccessToken = 30 * 60 * 1000; //for development
