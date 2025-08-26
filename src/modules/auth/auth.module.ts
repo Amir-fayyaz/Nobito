991e91b83f2c599b9d63d4eacb63ee0c8f0ce51d
= <fayamir54@gmail.com>
2025-08-26 06:30:42 -0700

    add new dependencies

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,16 +1,31 @@
 import { CacheService } from '@common/services/cache.service';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './controllers/auth.controller';
+import { Permission } from './entities/permission.entity';
+import { UserPermissions } from './entities/user-permissions.entity';
+import { UserRoles } from './entities/user-roles.entity';
 import { User } from './entities/user.entity';
 import { AuthService } from './services/auth.service';
+import { JwtAppService } from './services/jwt.service';
 import { OtpService } from './services/otp.service';
 import { UserService } from './services/user.service';
 
+@Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserRoles, Permission, UserPermissions]),
+  ],
   controllers: [AuthController],
-  providers: [UserService, CacheService, OtpService, AuthService],
-  exports: [],
+  providers: [
+    UserService,
+    CacheService,
+    OtpService,
+    AuthService,
+    JwtAppService,
+    JwtService,
+  ],
+  exports: [JwtAppService, JwtService, AuthService],
 })
 export class AuthModule {}
