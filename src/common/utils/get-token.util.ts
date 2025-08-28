61e8623968e938a9d217778525c63ddee5b788e9
= <fayamir54@gmail.com>
2025-08-28 16:49:05 -0700

    Fixed cookie-getter

--- src/common/utils/get-token.util.ts
+++ src/common/utils/get-token.util.ts
@@ -1,8 +1,10 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
 
-export function getAuthToken(
-  request: Request,
-  cookieName: string,
-): string | null {
-  return request.cookies.name[cookieName];
+export function getAuthToken(request: Request, cookieName: string): string {
+  const accessToken = request.cookies?.[cookieName];
+
+  if (!accessToken) throw new UnauthorizedException('token not provided');
+
+  return accessToken;
 }
