391a1214d9041cd7712411e2c03b504249f00cdb
= <fayamir54@gmail.com>
2025-08-28 13:22:50 -0700

    refactor otp service for type of otp`s

--- src/modules/auth/services/otp.service.ts
+++ src/modules/auth/services/otp.service.ts
@@ -1,17 +1,17 @@
+import { OtpType } from '@common/@types/otp-types';
 import { CacheService } from '@common/services/cache.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class OtpService {
   constructor(private readonly cacheService: CacheService) {}
-  async sendOtp(phone: string) {
-    const otp = this.generateOtp();
-
-    await this.cacheService.set(`phone:${phone}`, otp, 120);
-
-    //sms service for production
-
-    return { otp };
+  async sendOtp(type: OtpType, payload: string) {
+    switch (type) {
+      case (type = OtpType.PHONE):
+        return await this.sendOtpToPhone(payload);
+      case (type = OtpType.EMAIL):
+        return await this.sendOtpToEmail(payload);
+    }
   }
 
   private generateOtp(): string {
@@ -20,5 +20,24 @@ export class OtpService {
       otp += Math.floor(Math.random() * 10);
     }
     return otp;
+  }
+
+  private async sendOtpToPhone(phone: string) {
+    const otp = this.generateOtp();
+
+    await this.cacheService.set(`phone:${phone}`, otp, 120);
+
+    //sms service for production
+
+    return { otp };
+  }
+  private async sendOtpToEmail(email: string) {
+    const otp = this.generateOtp();
+
+    await this.cacheService.set(`email:${email}`, otp, 120);
+
+    //Email service for production
+
+    return { otp };
   }
 }
