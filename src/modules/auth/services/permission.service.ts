f0b3729bfcde1ca71aef2274d430eeccc44b1fcc
= <fayamir54@gmail.com>
2025-08-28 10:00:39 -0700

    add update-permission service

--- src/modules/auth/services/permission.service.ts
+++ src/modules/auth/services/permission.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePermissionDto } from '../dto/create-permission.dto';
+import { UpdatePermissionDto } from '../dto/update-permission.dto';
 import { Permission } from '../entities/permission.entity';
 
 @Injectable()
@@ -28,5 +29,13 @@ export class PermissionService {
       where: { id },
       relations: { permissionCategory: true },
     });
+  }
+
+  async update(id: string, dto: UpdatePermissionDto) {
+    const { affected } = await this.permissionRepository.update(id, dto);
+
+    if (!affected) throw new NotFoundException('permission not found');
+
+    return { success: true };
   }
 }
