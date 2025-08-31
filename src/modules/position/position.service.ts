2bb10a4e571b17a5a700334bcec6e97525425b19
= <fayamir54@gmail.com>
2025-08-31 15:50:31 -0700

    add create-position service

---
+++ src/modules/position/position.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePositionDto } from './dto/create-position.dto';
+import { Position } from './entities/position.entity';
+
+@Injectable()
+export class PositionService {
+  constructor(
+    @InjectRepository(Position)
+    private readonly positionRepository: Repository<Position>,
+  ) {}
+
+  async create(dto: CreatePositionDto): Promise<Position> {
+    const newPosition = this.positionRepository.create(dto);
+
+    return await this.positionRepository.save(newPosition);
+  }
+}
