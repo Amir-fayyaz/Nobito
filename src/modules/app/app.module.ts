ceef5981832862141d89125e20a63de54eeb8f0b
= <fayamir54@gmail.com>
2025-08-31 15:49:04 -0700

    add position-module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { RedisModule } from '@common/modules/redis.module';
 import { TypeOrmConfig } from '@config/typeorm.config';
 import { AuthModule } from '@module/auth/auth.module';
 import { AuthorizationGuard } from '@module/auth/guards/auth.guard';
+import { PositionModule } from '@module/position/position.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
@@ -17,6 +18,7 @@ import { AppService } from './app.service';
     AuthModule,
     AppCacheModule,
     RedisModule.forRootAsync(),
+    PositionModule,
   ],
   controllers: [AppController],
   providers: [
