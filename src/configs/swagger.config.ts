3a9ab71d9c210efc5501c399c842913e356d0ee0
= <fayamir54@gmail.com>
2025-10-13 06:05:17 -0700

    set-up swagger

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,15 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function Swagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('barbery api')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Swagger } from '@config/swagger.config';
 import { AppModule } from '@module/app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -19,6 +20,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  Swagger(app);
 
   await app.listen(Number(process.env.APP_PORT), async () => {
     Logger.log(
