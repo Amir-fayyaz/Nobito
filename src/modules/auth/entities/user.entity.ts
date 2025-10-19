6d5bd0f65d0960b2e8cb0c78cf9879cbd1310d51
= <fayamir54@gmail.com>
2025-10-20 02:00:22 +0330

    add user-entity

---
+++ src/modules/auth/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { BaseApplicationEntity } from '@common/entity/base.entity';
+import { GenderEnum } from '@common/enums/gender.enum';
+import { Column, Entity, Index } from 'typeorm';
+
+@Entity()
+@Index(['firstName', 'lastName'])
+export class User extends BaseApplicationEntity {
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  @Column({ nullable: true, unique: true })
+  nationalCode: string;
+
+  @Column({ type: 'enum', enum: GenderEnum, nullable: true })
+  gender: GenderEnum;
+}
